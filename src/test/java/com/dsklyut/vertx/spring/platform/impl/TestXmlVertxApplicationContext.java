1f0d8bd0fb05fbf9812c810e05eb247ec961c0ac
Dmitry Sklyut <dmitry@dsklyut.com>
2013-04-01 11:18:01 -0400

    adding some tests

---
+++ src/test/java/com/dsklyut/vertx/spring/platform/impl/TestXmlVertxApplicationContext.java
@@ -0,0 +1,56 @@
+package com.dsklyut.vertx.spring.platform.impl;
+
+
+import com.dsklyut.vertx.spring.test.LifecycleTestingClass;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.vertx.java.core.Vertx;
+import org.vertx.java.core.json.JsonObject;
+import org.vertx.java.platform.Container;
+import org.vertx.java.platform.PlatformLocator;
+import org.vertx.java.platform.PlatformManager;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
+/**
+ * User: dsklyut
+ * Date: 4/1/13
+ * Time: 10:15 AM
+ */
+public class TestXmlVertxApplicationContext {
+
+    @Test
+    public void validateLoadingOfSimpleContext() {
+        JsonObject config = new JsonObject();
+        PlatformManager pm = PlatformLocator.factory.createPlatformManager();
+        Vertx vertx = pm.getVertx();
+        // there is no cntr that we can safely use (package private) so just mock for this test
+        Container mockContainer = Mockito.mock(Container.class);
+        when(mockContainer.getConfig()).thenReturn(config);
+
+        VertxApplicationContext cntx = new XmlVertxApplicationContext();
+        cntx.setVertx(vertx);
+        cntx.setContainer(mockContainer);
+        cntx.setConfigLocation("contexts/testLifecycleInterfaces.xml");
+
+        try {
+            cntx.refresh();
+
+            LifecycleTestingClass underTest = cntx.getBean("support", LifecycleTestingClass.class);
+            assertNotNull(underTest);
+            assertNotNull(underTest.getVertx());
+            assertNotNull(underTest.getConfig());
+            assertNotNull(underTest.getConfig());
+            assertNotNull(underTest.getEb());
+
+            assertEquals(config, underTest.getConfig());
+        } catch (Exception ex) {
+            fail(ex.getMessage());
+        } finally {
+            if (cntx.isActive()) {
+                cntx.close();
+            }
+        }
+    }
+}
---
+++ src/test/java/com/dsklyut/vertx/spring/test/LifecycleTestingClass.java
@@ -0,0 +1,72 @@
+package com.dsklyut.vertx.spring.test;
+
+import com.dsklyut.vertx.spring.ConfigAware;
+import com.dsklyut.vertx.spring.ContainerAware;
+import com.dsklyut.vertx.spring.EventBusAware;
+import com.dsklyut.vertx.spring.VertxAware;
+import org.springframework.beans.factory.InitializingBean;
+import org.vertx.java.core.Vertx;
+import org.vertx.java.core.eventbus.EventBus;
+import org.vertx.java.core.json.JsonObject;
+import org.vertx.java.platform.Container;
+
+import static org.springframework.util.Assert.notNull;
+
+/**
+ * User: dsklyut
+ * Date: 4/1/13
+ * Time: 10:00 AM
+ */
+public class LifecycleTestingClass implements VertxAware, ContainerAware, EventBusAware, ConfigAware, InitializingBean {
+
+
+    private JsonObject config;
+    private Container container;
+    private EventBus eb;
+    private Vertx vertx;
+
+
+    @Override
+    public void setConfig(JsonObject config) {
+        this.config = config;
+    }
+
+    @Override
+    public void setContainer(Container container) {
+        this.container = container;
+    }
+
+    @Override
+    public void setEventBus(EventBus eventBus) {
+        this.eb = eventBus;
+    }
+
+    @Override
+    public void setVertx(Vertx vertx) {
+        this.vertx = vertx;
+    }
+
+    public JsonObject getConfig() {
+        return config;
+    }
+
+    public Container getContainer() {
+        return container;
+    }
+
+    public EventBus getEb() {
+        return eb;
+    }
+
+    public Vertx getVertx() {
+        return vertx;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        notNull(config, "Config is null");
+        notNull(container, "Container is null");
+        notNull(vertx, "Vertx is null");
+        notNull(eb, "EventBus is null");
+    }
+}
